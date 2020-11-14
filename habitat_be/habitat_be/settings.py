8210b6c1a79254c4d76e1b36d5cc37b13bd5cf16
Ivan Kljun <ivankljun@gmail.com>
2020-11-14 11:41:49 +0100

    first commit

--- habitat_be/habitat_be/settings.py
+++ habitat_be/habitat_be/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6z=j$masrbzyp!+=q04vtt4ew95wn*1hmh3p##)ku(i)vsy)o0'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
